7d389430750ffe72024d7968444032fc153f8254
Chandler May <cjmay4754@gmail.com>
2010-12-01 14:07:31 -0800

    Changed correct_east1_factor.py to automatically commit (skip the transaction).  The East 1 monitoring script needs to be stopped for the correction to do what we expect, anyway; the transaction just complicates things.

--- dev/correct_east1_factor.py
+++ dev/correct_east1_factor.py
@@ -7,12 +7,14 @@ The multiplier for East 1 was discovered to be too low (by a factor of
 stored readings, then recreating the averages in power_averages.
 
 Ensure the East 1 monitoring script is stopped before running this!
+Otherwise things may get *very* messy.
 '''
 
 
 import sys
 sys.path.append('/var/local/energy/lib')
 import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from energyconfig import *
 
 
@@ -22,12 +24,12 @@ EAST1_FACTOR_FACTOR = 10 # multiply the current factor by 10
 
 def main():
     conn = psycopg2.connect(PSQL_CONNSTR)
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
     cur = conn.cursor()
 
     # Update the factor
     cur.execute('''UPDATE sensors SET factor = factor * %s WHERE id = %s;''',
                 (EAST1_FACTOR_FACTOR, EAST1_SENSOR_ID))
-    conn.commit()
 
     # Update sensor_readings
     cur.execute('''UPDATE sensor_readings 
@@ -46,12 +48,10 @@ def main():
                  EAST1_FACTOR_FACTOR, EAST1_FACTOR_FACTOR, EAST1_FACTOR_FACTOR,
                  EAST1_FACTOR_FACTOR, EAST1_FACTOR_FACTOR, EAST1_FACTOR_FACTOR,
                  EAST1_SENSOR_ID))
-    conn.commit()
 
     # "Update" power_averages by deleting and recreating
     cur.execute('''DELETE FROM power_averages WHERE sensor_id = %s;''',
                 (EAST1_SENSOR_ID,))
-    conn.commit()
     cur.execute('''INSERT INTO power_averages (rdngtime_first, 
                      rdngtime_last, sensor_id, avg_type, num_pts, watts) 
                    SELECT MIN(rdngtime), MAX(rdngtime), %s, %s, COUNT(*), 
@@ -60,7 +60,6 @@ def main():
                      WHERE sensor_id = %s
                      GROUP BY date_trunc('month', rdngtime);''',
                 (EAST1_SENSOR_ID, AVG_TYPE_MONTH, EAST1_SENSOR_ID))
-    conn.commit()
     cur.execute('''INSERT INTO power_averages (rdngtime_first, 
                      rdngtime_last, sensor_id, avg_type, num_pts, watts) 
                    SELECT MIN(rdngtime), MAX(rdngtime), %s, %s, COUNT(*), 
@@ -69,7 +68,6 @@ def main():
                      WHERE sensor_id = %s
                      GROUP BY date_trunc('week', rdngtime);''',
                 (EAST1_SENSOR_ID, AVG_TYPE_WEEK, EAST1_SENSOR_ID))
-    conn.commit()
 
 
 if __name__ == '__main__':
