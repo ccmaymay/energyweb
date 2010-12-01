0e66e634ed266301f558544923f06a6a3b19e61d
Chandler May <cjmay4754@gmail.com>
2010-11-30 23:44:05 -0800

    Updated week1.sql to round the averages.

--- dev/week1.sql
+++ dev/week1.sql
@@ -1,5 +1,5 @@
 SELECT sensor_groups.name AS dorm, 
-       SUM(power_averages.watts / 1000) AS kW, 
+       ROUND(CAST (SUM(power_averages.watts / 1000) AS numeric), 3) AS kW, 
        MIN(power_averages.rdngtime_first) AS first_record, 
        MAX(power_averages.rdngtime_last) AS last_record,
        SUM(num_pts) AS num_data_points
@@ -8,6 +8,6 @@ SELECT sensor_groups.name AS dorm,
     INNER JOIN sensor_groups on (sensors.sensor_group_id = sensor_groups.id) 
   WHERE power_averages.avg_type = 1 
     AND power_averages.rdngtime_first >= '2010-11-01 00:00:00' 
-    AND power_averages.rdngtime_first < (timestamp '2010-11-01 00:00:00' + interval '1 week')
+    AND power_averages.rdngtime_first < '2010-11-08 00:00:00'
   GROUP BY sensor_groups.name 
   ORDER BY sensor_groups.name ASC;
